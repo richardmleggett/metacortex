35a3af5945adc79f2f140e919879cd148f6bdd52
aylingm <Martin.Ayling@tgac.ac.uk>
2016-04-13 12:39:24 +0100

    modified:   include/cortex/graph_stats.h 	modified:   src/cortex/graph_stats.c 	modified:   src/cortex/metagraphs.c

--- src/cortex/graph_stats.c
+++ src/cortex/graph_stats.c
@@ -116,51 +116,54 @@
                 // is it blunt? has it been seen before?
                 // things I@m dropping for now - loop detection, branch + loop, catching too large a bubble
                 if (db_node_is_blunt_end_all_colours(end_node, end_orientation)) {
-                // DO NOTHING WITH THIS
-          				//db_graph_check_and_add_path(merged_path, patharray);
+                  // DO NOTHING WITH THIS
+                  //db_graph_check_and_add_path(merged_path, patharray);
+                }
+                if (db_node_check_flag_visited(end_node)) {
+                   // need to count back from here to original branching point?
+                }
+
+               // Now go through all nodes, look for best and mark all as visited
+               for (i=0; i<new_path->length; i++) {
+                   if (!db_node_check_flag_visited(new_path->nodes[i])) {
+                       int this_coverage = element_get_coverage_all_colours(new_path->nodes[i]);
+                       int this_FOR_edges = db_node_edges_count_all_colours(new_path->nodes[i], forward);
+                       int this_REV_edges = db_node_edges_count_all_colours(new_path->nodes[i], reverse);
+
+                       // add node degrees to 2D array of all degrees in subgraph
+                       nodes_in_graph->node_degree[this_FOR_edges][this_REV_edges]++;
+
+                       if ((best_node == 0) ||
+                           (this_coverage > best_coverage) ||
+                           ((this_coverage == best_coverage) && ((this_FOR_edges + this_REV_edges) < best_edges)))
+                       {
+                           best_coverage = this_coverage;
+                           best_edges = (this_FOR_edges + this_REV_edges);
+                           *best_node = new_path->nodes[i];
+                       }
+
+                      if (db_node_check_for_any_flag(new_path->nodes[i], BRANCH_NODE_FORWARD)){
+                        nodes_in_graph->branch_nodes++;
+                        //nodes_in_graph->Y_degree_for[this_FOR_edges-1]++;
+                      }
+                      else if (db_node_check_for_any_flag(new_path->nodes[i], BRANCH_NODE_REVERSE)){
+                        nodes_in_graph->branch_nodes++;
+                      //  nodes_in_graph->Y_degree_rev[this_REV_edges-1]++;
+                      }
+                      else if (db_node_check_for_any_flag(new_path->nodes[i], X_NODE)){
+                        nodes_in_graph->branch_nodes++;
+                        // no ideal way to do this. Could distinguish between different classes of x-node, but maybe enough to just think of densely connected ones
+                      //  nodes_in_graph->X_degrees[this_FOR_edges+this_REV_edges-1]++;
+                      }
+
+                       db_node_action_set_flag_visited(new_path->nodes[i]);
+                       queue_push(graph_queue, new_path->nodes[i]);
+                       nodes_in_graph->total_size++;
+                   }
                }
-               if (db_node_check_flag_visited(end_node)) {
-                 // need to count back from here to original branching point?
-               }
 
-                 // Now go through all nodes, look for best and mark all as visited
-                 for (i=0; i<new_path->length; i++) {
-                     if (!db_node_check_flag_visited(new_path->nodes[i])) {
-                         int this_coverage = element_get_coverage_all_colours(new_path->nodes[i]);
-                         int this_FOR_edges = db_node_edges_count_all_colours(new_path->nodes[i], forward);
-                         int this_REV_edges = db_node_edges_count_all_colours(new_path->nodes[i], reverse);
-
-                         if ((best_node == 0) ||
-                             (this_coverage > best_coverage) ||
-                             ((this_coverage == best_coverage) && ((this_FOR_edges + this_REV_edges) < best_edges)))
-                         {
-                             best_coverage = this_coverage;
-                             best_edges = (this_FOR_edges + this_REV_edges);
-                             *best_node = new_path->nodes[i];
-                         }
-
-                        if (db_node_check_for_any_flag(new_path->nodes[i], BRANCH_NODE_FORWARD)){
-                          nodes_in_graph->branch_nodes++;
-                          nodes_in_graph->Y_degree_for[this_FOR_edges-1]++;
-                        }
-                        else if (db_node_check_for_any_flag(new_path->nodes[i], BRANCH_NODE_REVERSE)){
-                          nodes_in_graph->branch_nodes++;
-                          nodes_in_graph->Y_degree_rev[this_REV_edges-1]++;
-                        }
-                        else if (db_node_check_for_any_flag(new_path->nodes[i], X_NODE)){
-                          nodes_in_graph->branch_nodes++;
-                          // no ideal way to do this. Could distinguish between different classes of x-node, but maybe enough to just think of densely connected ones
-                          nodes_in_graph->X_degrees[this_FOR_edges+this_REV_edges-1]++;
-                        }
-
-                         db_node_action_set_flag_visited(new_path->nodes[i]);
-                         queue_push(graph_queue, new_path->nodes[i]);
-                         nodes_in_graph->total_size++;
-                     }
-                 }
-
-                 // Clean up
-                 path_destroy(new_path);
+               // Clean up
+               path_destroy(new_path);
              }
          }
      }
@@ -215,12 +218,12 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
 {
   FILE* fp_analysis;
 	//int branchNodes = 0;
-	int X_Nodes_tot[8];
-	int Y_Nodes_rev[4];
-	int Y_Nodes_for[4];
+	//int X_Nodes_tot[8];
+	//int Y_Nodes_rev[4];
+	//int Y_Nodes_for[4];
   long int Contig_Branches[MAX_BRANCHES];
-	int X_Nodes = 0;
-	int Y_Nodes = 0;
+	//int X_Nodes = 0;
+	//int Y_Nodes = 0;
   char* seq = calloc(256, 1);
   long int total_nodes = 0;
   GraphInfo* nodes_in_graph;
@@ -230,7 +233,7 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
   char analysis_filename[strlen(consensus_contigs_filename) + 10];
 
   Queue* graph_queue;
-  int i;
+  int i;  int j;
   // Initialise Coverage_Dist  int i;
   for(i=0;i<MAX_BRANCHES;i++){
     Contig_Branches[i]=0;
@@ -238,12 +241,12 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
   for(i=0;i<COVERAGE_BINS;i++){
     Coverage_Dist[i]=0;
   }
-  for(i=0;i<4;i++){
-    X_Nodes_tot[i*2]=0;
-    X_Nodes_tot[(i*2)+1]=0;
-    Y_Nodes_for[i]=0;
-    Y_Nodes_rev[i]=0;
-  } // should I be using pointers and malloc/calloc instead?
+  //for(i=0;i<4;i++){
+  //  X_Nodes_tot[i*2]=0;
+  //  X_Nodes_tot[(i*2)+1]=0;
+  //  Y_Nodes_for[i]=0;
+  //  Y_Nodes_rev[i]=0;
+  //} // should I be using pointers and malloc/calloc instead?
 
   /* Open the analysis file */
   sprintf(analysis_filename, "%s.analysis", consensus_contigs_filename);
@@ -278,22 +281,22 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
 		if (edges_forward > 1
 		    && edges_reverse == 1) {
 			db_node_action_set_flag(node, BRANCH_NODE_FORWARD);
-      Y_Nodes_for[edges_forward - 1]++;
-      Y_Nodes++;
+      //Y_Nodes_for[edges_forward - 1]++;
+      //Y_Nodes++;
 		}
 		// Look for Y shape branch reverse orientation
 		if (edges_reverse > 1
 		    && edges_forward == 1) {
 			db_node_action_set_flag(node, BRANCH_NODE_REVERSE);
-      Y_Nodes_rev[edges_reverse - 1]++;
-      Y_Nodes++;
+      //Y_Nodes_rev[edges_reverse - 1]++;
+      //Y_Nodes++;
 		}
 		// Look for X-shaped branch
 		if (edges_reverse > 1
 		    && edges_forward > 1) {
 			db_node_action_set_flag(node, X_NODE);
-      X_Nodes_tot[edges_reverse + edges_forward + 1]++;
-      X_Nodes++;
+      //X_Nodes_tot[edges_reverse + edges_forward + 1]++;
+      //X_Nodes++;
 		}
   } // identify_branch_nodes()
 
@@ -313,12 +316,16 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
       nodes_in_graph->total_size = 0;
       nodes_in_graph->branch_nodes = 0;
       nodes_in_graph->end_nodes = 0;
-      for(i=0;i<4;i++){
+      for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+          nodes_in_graph->node_degree[i][j]=0;
+        }
+      }
+    /*for(i=0;i<4;i++){
         nodes_in_graph->Y_degree_rev[i] = 0;
         nodes_in_graph->Y_degree_for[i] = 0;
         nodes_in_graph->X_degrees[i] = 0;
-        nodes_in_graph->X_degrees[i+4] = 0;
-      }
+      }*/
       // Grow graph from this node, returning the 'best' (highest coverage) node to store as seed point
       log_printf("Growing graph from node\n");
       graph_queue->number_of_items = 0;
@@ -355,14 +362,14 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
 	hash_table_traverse(&identify_branch_nodes, graph);
 
   // Output graph wide stats (X/Y node numbers)
-  fprintf(fp_analysis, "#total\t%li\n#X-nodes\t%i\n#Y-nodes\t%i\n\n",total_nodes, X_Nodes, Y_Nodes);
-  fprintf(fp_analysis, "#total\t%li\n\t\n\t#X\t#Y-FOR\t#Y-REV\n",total_nodes);
-  for(i=0;i<4;i++){
+  //fprintf(fp_analysis, "#total\t%li\n#X-nodes\t%i\n#Y-nodes\t%i\n\n",total_nodes, X_Nodes, Y_Nodes);
+  //fprintf(fp_analysis, "#total\t%li\n\t\n\t#X\t#Y-FOR\t#Y-REV\n",total_nodes);
+  /*for(i=0;i<4;i++){
     fprintf(fp_analysis, "%i\t%i\t%i\t%i\n",i, X_Nodes_tot[i], Y_Nodes_for[i], Y_Nodes_rev[i]);
   }
   for(i=4;i<8;i++){
     fprintf(fp_analysis, "%i\t%i\n",i, X_Nodes_tot[i]);
-  }
+  }*/
   // first line for stats output file
   fprintf(fp_analysis, "\n#Subgraph sizes\n");
 
@@ -388,18 +395,18 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
 
 void log_and_screen_print_stats(GraphInfo * nodes_in_graph){
   int i=0;
-  int X_PRESENT=0;
-  log_printf("\n#Complexity_dist of sub graph (# X/Y nodes)\t---\n");
+  //int X_PRESENT=0;
+/*  log_printf("\n#Complexity_dist of sub graph (# X/Y nodes)\t---\n");
   log_printf("DEG\tFOR\tREV\n");
   for(i=0;i<4;i++){
     log_printf("%i\t%i\t%i\n",i+1, nodes_in_graph->Y_degree_for[i], nodes_in_graph->Y_degree_rev[i]);
-  }
+  }*/
   log_printf("DEG\tALL\n");
-  for(i=0;i<8;i++){
+  /*for(i=0;i<8;i++){
     log_printf("%i\t%i\n",i+1, nodes_in_graph->X_degrees[i]);
     X_PRESENT=X_PRESENT+nodes_in_graph->X_degrees[i];
-  }
+
   if(X_PRESENT){
     log_printf("X_PRESENT\t%i\n", X_PRESENT);
-  }
+  }}*/
 }
