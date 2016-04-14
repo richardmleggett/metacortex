89f9fbf23fa33b65e3dc5198ed11825691ea270d
aylingm <Martin.Ayling@tgac.ac.uk>
2016-04-14 10:50:51 +0100

    fixed memory stamp - calloc-ing 'nodes_in_graph' 	modified:   src/cortex/graph_stats.c

--- src/cortex/graph_stats.c
+++ src/cortex/graph_stats.c
@@ -214,7 +214,13 @@ void find_subgraph_stats(dBGraph * graph, char* consensus_contigs_filename)
   long int Contig_Branches[MAX_BRANCHES];
   char* seq = calloc(256, 1);
   long int total_nodes = 0;
-  GraphInfo* nodes_in_graph;
+
+  GraphInfo* nodes_in_graph = calloc(1,sizeof(GraphInfo));
+  //nodes_in_graph->total_size = malloc (1);
+  //nodes_in_graph->total_size = malloc (1);
+  //nodes_in_graph->branch_nodes = malloc (1);
+  //nodes_in_graph->end_nodes = calloc (5, 5);
+
   // array to bin coverage 0-5, 5-10, 10-15..95-100
   long int Coverage_Dist[COVERAGE_BINS]; // will this work?
   char analysis_filename[strlen(consensus_contigs_filename) + 10];
